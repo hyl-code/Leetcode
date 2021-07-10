99c8a484730389b93cebbbfb0649dae40e586bfc
hyl-code <2839797603@qq.com>
2021-07-10 10:07:00 +0800

    7.10

---
+++ src/July_2021/MergeTwoLists.java
@@ -0,0 +1,28 @@
+package July_2021;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+public class MergeTwoLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+   }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+
+        ListNode res = l1.val < l2.val ? l1 : l2;
+        res.next = mergeTwoLists(res.next, l1.val >= l2.val ? l1 : l2);
+        return res;
+    }
+}
