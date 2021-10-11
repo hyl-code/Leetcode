73741552df6154be5b6abf3a9022576e74528cc3
hyl-code <2839797603@qq.com>
2021-10-12 00:06:50 +0800

    10.11

---
+++ src/October_2021/containsNearbyDuplicate.java
@@ -0,0 +1,20 @@
+package October_2021;
+
+/*
+    给定一个整数数组和一个整数 k, 判断数组中是否存在两个不同的索引 i 和 j,
+    使得 nums [i] = nums [j],并且 i 和 j 的差的绝对值至多为k.
+ */
+
+public class containsNearbyDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums.length < 2)
+            return false;
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j <= i + k && j < nums.length; j++){
+                if(nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
