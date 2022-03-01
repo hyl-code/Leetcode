4ab8bb72e65a716b67ddc4cc0604d7f3b8601c8e
hyl-code <2839797603@qq.com>
2022-03-01 14:36:21 +0800

    3.1

--- src/March_2022/merge_56.java
+++ src/March_2022/merge_56.java
@@ -7,16 +7,21 @@ import java.util.Arrays;
 
 public class merge_56 {
     public int[][] merge(int[][] intervals) {
+        //按照区间的起始位置排序
         Arrays.sort(intervals,(v1, v2) -> v1[0] - v2[0]);
         int[][] res = new int[intervals.length][2];
         int index = -1;
+        //遍历区间
         for(int[] interval : intervals){
+            //如果结果数组为空或当前区间对起始位置大于数组最后区间的终止位置, 则不合并, 直接加入结果数组
+            //反之, 合并数组
             if(index == -1 || interval[0] > res[index][1]){
                 res[++index] = interval;
             }else{
                 res[index][1] = Math.max(res[index][1], interval[1]);
             }
         }
+        //Arrays.copyOf()使得返回数组不含多余数组[0, 0]
         return Arrays.copyOf(res, index + 1);
     }
 }
