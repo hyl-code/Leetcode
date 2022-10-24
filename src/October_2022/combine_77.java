89e5e23282832687cf87610bbdb1efbcc60987c0
hyl-code <2839797603@qq.com>
2022-10-24 11:15:52 +0800

    10.24

---
+++ src/October_2022/combine_77.java
@@ -0,0 +1,31 @@
+package October_2022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    给定两个整数 n 和 k, 返回范围 [1, n] 中所有可能的 k 个数的组合.
+    你可以按任何顺序返回答案.
+ */
+public class combine_77 {
+    //本质: 树的构造
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        getCombine(n, k, 1, new ArrayList<>());
+        return res;
+    }
+
+    public void getCombine(int n, int k, int start, List<Integer> list){
+        if(k == 0){
+            //如果为 0 则代表 list 中已经存入 k 个数, 将 list 存入结果 res 中
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i = start; i <= n - k + 1; i++){
+            list.add(i);
+            getCombine(n, k - 1, i + 1, list);
+            //回溯操作(回退处理结点[1, 2]撤销 2, [1, 3]撤销 3)
+            list.remove(list.size() - 1);
+        }
+    }
+}
--- src/October_2022/partitionDisjoint_915.java
+++ src/October_2022/partitionDisjoint_915.java
@@ -1,5 +1,30 @@
 package October_2022;
 
+/*
+    给定一个数组 nums, 将其划分为两个连续子数组 left 和 right, 使得:
+    left 中的每个元素都小于或等于 right 中的每个元素.
+    left 和 right 都是非空的.
+    left 的长度要尽可能小.
+    在完成这样的分组后返回 left 的长度.
+    用例可以保证存在这样的划分方法.
+ */
 public class partitionDisjoint_915 {
-    
+    public int partitionDisjoint(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int index = 0;
+        //当前最大值
+        int max = nums[0];
+        //左数组最大值
+        int leftMax = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max, nums[i]);
+            if(nums[i] < leftMax){
+                leftMax = max;
+                index = i;
+            }
+        }
+        return index + 1;
+    }
 }
