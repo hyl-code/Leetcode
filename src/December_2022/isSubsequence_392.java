7b3105667890749bc822bfe0d4dd012ccdaf4a19
hyl-code <2839797603@qq.com>
2022-12-10 19:36:20 +0800

    12.10

---
+++ src/December_2022/isSubsequence_392.java
@@ -0,0 +1,19 @@
+package December_2022;
+
+/*
+    给定字符串 s 和 t, 判断 s 是否为 t 的子序列.
+    字符串的一个子序列是原始字符串删除一些(也可以不删除)字符而不改变剩余字符相对位置形成的新字符串.
+    (例如, "ace"是 "abcde" 的一个子序列, 而"aec"不是).
+ */
+public class isSubsequence_392 {
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for(char c : s.toCharArray()){
+            index = t.indexOf(c, index + 1);
+            if(index == -1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
