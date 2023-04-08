39c06b99e2037642e2ea8b6faffae359c861fc9c
hyl-code <2839797603@qq.com>
2023-04-08 23:54:11 +0800

    4.8

---
+++ src/April_2023/fourSum_18.java
@@ -0,0 +1,67 @@
+package April_2023;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+    给你一个由 n 个整数组成的数组 nums, 和一个目标值 target.
+    请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]]
+    (若两个四元组元素一一对应，则认为两个四元组重复):
+        0 <= a, b, c, d < n
+        a, b, c 和 d 互不相同
+        nums[a] + nums[b] + nums[c] + nums[d] == target
+    你可以按任意顺序返回答案.
+ */
+public class fourSum_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length < 4){
+            return list;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            if((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target){
+                break;
+            }
+            if((long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target){
+                continue;
+            }
+            for(int j = i + 1; j < n - 2; j ++ ){
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                if((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target){
+                    break;
+                }
+                if((long) nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target){
+                    continue;
+                }
+                int left = j + 1, right = n - 1;
+                while(left < right){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
+                        list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        }
+                        left++;
+                        while(left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        right--;
+                    }else if(sum < target){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
