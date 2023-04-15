5e150e766af167e678ae0d14da6f98774f381768
hyl-code <2839797603@qq.com>
2023-04-15 12:54:13 +0800

    4.15

--- src/April_2023/gardenNoAdj_1042.java
+++ src/April_2023/gardenNoAdj_1042.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
-
+    有 n 个花园, 按从 1 到 n 标记. 另有数组 paths, 其中 paths[i] = [xi, yi] 描述了花园 xi 到花园 yi 的双向路径.
+    在每个花园中, 你打算种下四种花之一. 另外，所有花园最多有 3 条路径可以进入或离开.
+    你需要为每个花园选择一种花,使得通过路径相连的任何两个花园中的花的种类互不相同.
+    以数组形式返回任一可行的方案作为答案 answer, 其中 answer[i] 为在第 (i+1) 个花园中种植的花的种类.
+    花的种类用 1, 2, 3, 4 表示. 保证存在答案.
  */
 public class gardenNoAdj_1042 {
     public int[] gardenNoAdj(int n, int[][] paths) {
@@ -23,7 +27,9 @@ public class gardenNoAdj_1042 {
                 used[color[j]] = true;
             }
             // 当 used[color[i]] == false 时, 则找到 color 为同种类花所处花园不相邻
-            while(used[++color[i]]);
+            while(used[color[i]]){
+                color[i]++;
+            }
         }
         return color;
     }
