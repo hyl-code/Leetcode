4b4f2b6992a84f88162792aaea768255b3e241ef
hyl-code <2839797603@qq.com>
2023-07-02 15:02:52 +0800

    7.2

---
+++ src/July_2023/twoSum_1.java
@@ -0,0 +1,29 @@
+package July_2023;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{ i, j };
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    // HashMap
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{ map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
