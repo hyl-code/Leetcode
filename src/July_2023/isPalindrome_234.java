271e6819754308daec19d49fd1d7c825d679805c
hyl-code <2839797603@qq.com>
2023-07-29 20:48:37 +0800

    7.29

---
+++ src/July_2023/isPalindrome_234.java
@@ -0,0 +1,49 @@
+package July_2023;
+
+/*
+    给你一个单链表的头节点 head, 请你判断该链表是否为回文链表.
+    如果是, 返回 true; 否则, 返回 false.
+ */
+public class isPalindrome_234 {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow = reverse(slow.next);
+        while(slow != null){
+            if(head.val != slow.val){
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    // 反转链表
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode next = null;
+        while(head != null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
