07a28a60c27793767a733175f30e7e0f2b6f2c5f
hyl-code <2839797603@qq.com>
2023-09-05 17:20:34 +0800

    9.5

---
+++ src/August_2023/minNumber_2605.java
@@ -0,0 +1,27 @@
+package August_2023;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+/*
+    给你两个只包含 1 到 9 之间数字的数组 nums1 和 nums2, 每个数组中的元素互不相同.
+    请你返回最小的数字, 两个数组都至少包含这个数字的某个数位.
+ */
+public class minNumber_2605 {
+    public int minNumber(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        int min1 = 10, min2 = 10;
+        for(int num : nums1){
+            set.add(num);
+            min1 = Math.min(min1, num);
+        }
+        Arrays.sort(nums2);
+        for(int num : nums2){
+            if(set.contains(num)){
+                return num;
+            }
+            min2 = Math.min(min2, num);
+        }
+        return Math.min(min1, min2) * 10 + Math.max(min1, min2);
+    }
+}
