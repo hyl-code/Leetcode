ded5cd94921a5dc3a84daa84534be00bc6731d90
hyl-code <2839797603@qq.com>
2024-01-04 23:43:20 +0800

    1.4

---
+++ src/January_2024/maximumRows_2397.java
@@ -0,0 +1,40 @@
+package January_2024;
+
+/*
+    给你一个下标从 0 开始、大小为 m x n 的二进制矩阵 matrix; 另给你一个整数 numSelect, 表示你必须从 matrix 中选择的不同列的数量.
+    如果一行中所有的 1 都被你选中的列所覆盖, 则认为这一行被覆盖了.
+    形式上, 假设 s = {c1, c2, ...., cnumSelect} 是你选择的列的集合.
+    对于矩阵中的某一行 row, 如果满足下述条件, 则认为这一行被集合 s 覆盖：
+        对于满足 matrix[row][col] == 1 的每个单元格 matrix[row][col]（0 <= col <= n - 1）,
+        col 均存在于 s 中, 或者 row 中不存在值为 1 的单元格.
+    你需要从矩阵中选出 numSelect 个列, 使集合覆盖的行数最大化.
+    返回一个整数, 表示可以由 numSelect 列构成的集合覆盖的最大行数.
+ */
+public class maximumRows_2397 {
+    public int maximumRows(int[][] matrix, int numSelect) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] mask = new int[m];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                mask[i] += matrix[i][j] << (n - j - 1);
+            }
+        }
+        int res = 0;
+        int cur = 0;
+        int limit = (1 << n);
+        while(++cur < limit){
+            if(Integer.bitCount(cur) != numSelect){
+                continue;
+            }
+            int t = 0;
+            for(int j = 0; j < m; j++){
+                if((mask[j] & cur) == mask[j]){
+                    ++t;
+                }
+            }
+            res = Math.max(res, t);
+        }
+        return res;
+    }
+}
